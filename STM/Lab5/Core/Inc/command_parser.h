cf6c1c04c1688f1b2c9d28df1bd2c063dbbd2b7e
AT <nguyentronganhtuan2102@gmail.com>
2022-12-19 14:13:48 +0700

    Update Lab5

---
+++ STM/Lab5/Core/Inc/command_parser.h
@@ -0,0 +1,16 @@
+/*
+ * command_parser.h
+ *
+ *  Created on: Dec 18, 2022
+ *      Author: A.Tuan
+ */
+
+#ifndef INC_COMMAND_PARSER_H_
+#define INC_COMMAND_PARSER_H_
+#include "global.h"
+
+
+void command_parser_fsm();
+
+
+#endif /* INC_COMMAND_PARSER_H_ */
---
+++ STM/Lab5/Core/Inc/uart_communication.h
@@ -0,0 +1,16 @@
+/*
+ * uart_communication.h
+ *
+ *  Created on: Dec 18, 2022
+ *      Author: A.Tuan
+ */
+
+#ifndef INC_UART_COMMUNICATION_H_
+#define INC_UART_COMMUNICATION_H_
+#include "global.h"
+#include "software_timer.h"
+
+void uart_communication_fsm();
+
+
+#endif /* INC_UART_COMMUNICATION_H_ */
